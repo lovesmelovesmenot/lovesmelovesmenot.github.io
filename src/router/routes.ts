276cdb0c721d6e032f5c504c9127f2b69778f68f
A. Sefa Aslan <abidinsefa@outlook.com>
2024-01-03 00:24:17 +0300

    added app-ads

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,11 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
